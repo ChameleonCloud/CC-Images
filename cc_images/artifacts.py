77543c7022600136401b04617a757aab6ba465cd
Michael Sherman <shermanm@uchicago.edu>
2023-11-08 21:16:39 +0000

    allow builds without authentication to tacc
    to avoid duplicate work, artifacts.py attempts to list already
    existing images from tagg. However, a user may not care about this,
    and it introduces a dependency on authentication and access to tacc.
    
    This change just sets the value to Null if the connection fails, but future
    changes should depend only on public images, or instead use a dedicated cache
    configuration of some kind.

--- cc_images/artifacts.py
+++ cc_images/artifacts.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import openstack
+from openstack.exceptions import ConfigException
 
 
 class BuildArtifact:
@@ -12,11 +13,22 @@ class BuildArtifact:
     def __init__(self, name: str, obj: str, site: str):
         self.name = name
         self.obj = obj.strip("/ ")
-        self.conn = openstack.connect(cloud=site)
+        try:
+            self.conn = openstack.connect(cloud=site)
+        except ConfigException as ex:
+            self.conn = None
+
 
     @property
     def download_url(self) -> Union[str, None]:
+        try:
+            return_url = self.conn.object_store.generate_temp_url(
             path=f"/v1/AUTH_{self.conn.current_project_id}/{self.obj}",
             seconds=60 * 60,
             method="GET",
         )
+        except ConfigException as ex:
+            return None
+        else:
+            return return_url
+
